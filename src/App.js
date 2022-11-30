21dbcfbf3aed74db85078a40d0a6cd6b3ab4ba85
Eshitayasmin <eshita.emc2k17@gmail.com>
2022-11-30 21:20:43 +0600

    all select number calculate done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 function App() {
     //state
     const [divCount, setDivCount] = useState(0);
+    const [check, setCheck] = useState(false);
 
     const appendChilddiv = (e) => {
       e.preventDefault()
@@ -15,26 +16,36 @@ function App() {
    
     const handleSelect =()=>{
       let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-      console.log(checkboxes);
+      let inputNumbers = document.querySelectorAll('input[type="number"]');
+      console.log(inputNumbers);
       for(let checkbox of checkboxes){
          if(checkbox.checked===false){
           checkbox.checked = true;
           document.getElementById("allSelect").checked= true;
+          document.getElementById("calculate").style.display="block";
          }
          else{
           checkbox.checked = false;
           document.getElementById("allSelect").checked= false;
-         }
-         
+          document.getElementById("calculate").style.display="none";
+         } 
+      }
+      let total=0;
+      for(let number of inputNumbers){
+       total= total+ parseFloat(number.value); 
+       console.log(total);
+       document.getElementById("total").innerHTML=total;
       }
       
     }
+
+   
   return (
     <div className="App">
  <form>
         <input
           id="field"
-          type="number"
+          type="text"
         />
         <button onClick={appendChilddiv} type="submit">submit</button>
       </form>
@@ -48,10 +59,10 @@ function App() {
             .map((x, id) => (
               <div key={id}>
                 <input type="checkbox" name="" id="" />
-                <input type="number" name="" id=""/>
+                <input type="number" name="number" className="f"/>
               </div>
             ))}
-  
+  <p id="calculate">Selected all {divCount} items and Total number is <span id="total"></span></p>
       </div>
     </div>
   );
